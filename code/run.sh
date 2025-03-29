36045d8295a4716f27d66b16002608081dfa140e
ChaoqiJia <158010329+ChaoqiJia@users.noreply.github.com>
2025-03-30 00:15:18 +1100

    Create run.sh
    run

---
+++ code/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#check
+if [ $# -lt 1 ]; then
+  echo "用法: $0 <inputFilename>"
+  exit 1
+fi
+
+inputFilename="$1"
+
+# set CPLEX PATH
+export CPLEX_HOME="/Applications/CPLEX_Studio2211/cplex"
+
+# set CLASSPATH
+export CLASSPATH=".:$CPLEX_HOME/lib/cplex.jar"
+
+javac -cp "$CLASSPATH" addConstraints/OutPut.java
+
+# set dataset
+java -cp "$CLASSPATH" \
+  -Djava.library.path="$CPLEX_HOME/bin/arm64_osx" \
+  addConstraints.OutPut "$inputFilename"
