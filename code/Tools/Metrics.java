ab6b592c677716cdcd20088b278957bf07a02f69
ChaoqiJia <158010329+ChaoqiJia@users.noreply.github.com>
2025-04-04 20:27:27 +1100

    Add files via upload

---
+++ code/Tools/Metrics.java
@@ -0,0 +1,129 @@
+package Tools;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import static Tools.tools.distance;
+
+public class Metrics {
+    static float[][] assess = null;
+    public static String[] desc = {"Purity: ", ", NMI: ", ", RI: ", ", cost: ", ", whole time: ", ", selected center time: ", ", RDS time: "};
+
+
+    public static float[] metrics(int N, ArrayList<Point> pointlist, ArrayList<Integer> centers, int k, String times) {
+        assess = new float[k][k];
+        float maxDist = 0;
+
+        for (int i = 0; i < N; i++) {
+            maxDist = Math.max(maxDist, distance(pointlist.get(i), pointlist.get(centers.get(pointlist.get(i).getClusterID()))));
+//            if (maxDist > 100.0)
+//                System.out.println(pointlist.get(i));
+
+            //label and assignment
+            int Pmark = pointlist.get(i).getLabel() % k;
+            int Pcluster = pointlist.get(i).getClusterID();
+            assess[Pcluster][Pmark]++;
+        }
+        String result =  Purity(N) + ", " + NMI(N) + ", " + RI(N) + ", " + costFunction(maxDist) + ", " + times;
+        String[] data = result.split("[,]");
+        float[] turnDouble = new float[data.length];
+
+        for (int j = 0; j < data.length; j++) {
+            float value = Float.parseFloat(data[j].replaceAll("[\\s+]", ""));
+            BigDecimal bd = new BigDecimal(Float.toString(value));
+            bd = bd.setScale(5, RoundingMode.HALF_UP);
+            value = bd.floatValue();
+            turnDouble[j] = value;
+        }
+
+        System.out.println(toString(turnDouble, desc));
+        return turnDouble;
+    }
+
+
+    public static String toString(float[] metric, String[] desc) {
+        StringBuilder accuracy = new StringBuilder();
+
+        for (int i = 0; i < metric.length; i++) {
+            accuracy.append(desc[i]).append(metric[i]);
+        }
+        return accuracy + ",\n";
+    }
+
+
+    private static float Purity(int N) {
+        float testacc = 0;
+        for (float[] floats : assess) {
+            float maxAssess = 0;
+            for (float aFloat : floats) {
+                maxAssess = Math.max(aFloat, maxAssess);
+            }
+            testacc += maxAssess;
+        }
+        return testacc / N;
+    }
+    private static double log2(double a) {
+        return Math.log(a) / Math.log(2);
+    }
+
+    public static float NMI(int N) {
+
+        float[] assessI = new float[assess.length];
+        float[] assessJ = new float[assess.length];
+        float[][] p = new float[assess.length][assess.length];
+
+        for (int i = 0; i < assess.length; i++) {
+            for (int j = 0; j < assess[i].length; j++) {
+                p[i][j] = assess[i][j] / N;
+                assessI[i] += assess[i][j] / N;
+                assessJ[i] += assess[j][i] / N;
+            }
+        }
+
+        float IXY = 0, HX = 0, HY = 0;
+        for (int i = 0; i < assess.length; i++) {
+            for (int j = 0; j < assess[i].length; j++) {
+                if (p[i][j] != 0)
+                    IXY += p[i][j] * log2(p[i][j] / (assessI[i] * assessJ[j]));
+            }
+            if (assessI[i] != 0)
+                HX -= assessI[i] * log2(assessI[i]);
+            if (assessJ[i] != 0)
+                HY -= assessJ[i] * log2(assessJ[i]);
+        }
+        return 2.0f * IXY / (HX + HY);
+    }
+
+    private static float RI(int N) {
+        double testacc, com = 0;
+        for (float[] floats : assess) {
+            for (int j = 0; j < assess.length; j++) {
+                com += (floats[j] * (floats[j] - 1) / 2.0);
+            }
+        }
+
+        for (int i = 0; i < assess.length; i++) {
+            for (int j = 0; j < assess.length; j++) {
+                for (int l = i + 1; l < assess.length; l++) {
+                    for (int m = 0; m < assess.length; m++) {
+                        if (j != m) {
+                            com += (assess[i][j] * assess[l][m]);
+                        }
+                    }
+                }
+            }
+
+        }
+
+        testacc = (com * 2.0 / ((N - 1) * 1.0 * N));
+        System.out.println("RI: " + testacc);
+
+        return (float) testacc;
+    }
+
+
+    private static float costFunction(float maxDist) {
+        return maxDist;
+    }
+
+}
---
+++ code/Tools/load_data.java
@@ -0,0 +1,78 @@
+package Tools;
+
+import addConstraints.OutPut;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class load_data {
+    public static ArrayList<Point> readMyFile(String filePath, int index) {
+
+        if (filePath.contains("wine") || filePath.contains("cnae")) {
+            filePath = "/Users/jcq/Desktop/myPaper/TNNLS/ML-CL-k-Center-main/Datasets_all/" + filePath + ".data"; // your path
+            int markPosition = 0;
+            return loadWineData(filePath, markPosition, "[,]");
+        } else if (filePath.contains("skin")) {
+            filePath = "/Users/jcq/Desktop/myPaper/TNNLS/ML-CL-k-Center-main/Datasets_all/" + filePath + ".data"; // your path
+            int markPosition = 3;
+            return loadWineData(filePath, markPosition, "[\\s+]");
+        } else if (filePath.contains("covertype")) {
+            filePath = "/Users/jcq/Desktop/myPaper/TNNLS/ML-CL-k-Center-main/Datasets_all/" + filePath + ".data"; // your path
+            int markPosition = 54;
+            return loadWineData(filePath, markPosition, "[,]");
+        } else if (filePath.contains("wide09")) {
+            filePath = "/Users/jcq/Desktop/myPaper/TNNLS/ML-CL-k-Center-main/Datasets_all/" + filePath + ".data"; // your path
+            int markPosition = 21;
+            return loadWineData(filePath, markPosition, "[,]");
+        } else if (filePath.contains("kdd")) {
+            filePath = "/Users/jcq/Desktop/myPaper/TNNLS/ML-CL-k-Center-main/Datasets_all/" + filePath + ".data"; // your path
+            int markPosition = 41;
+            return loadWineData(filePath, markPosition, "[,]");
+        }else if (filePath.contains("simu")) {
+            filePath = "/Users/jcq/Desktop/myPaper/TNNLS/ML-CL-k-Center-main/Datasets_all/" + filePath + "/"+index+"-oriData.data"; // your path
+            int markPosition = 50;
+            return loadWineData(filePath, markPosition, "[,]");
+        }
+        return null;
+    }
+
+    public static ArrayList<Point> loadWineData(String filePath, int markPosition, String delimiter) {
+        ArrayList<Point> pointList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            int i = 0;
+            Set<Integer> label_numbers = new HashSet<>();
+//            Map<String, Integer> countMap = new HashMap<>();
+            while ((line = br.readLine()) != null) {
+                String[] values = Arrays.stream(line.split(delimiter))
+                        .map(String::trim)
+                        .filter(s -> !s.isEmpty())
+                        .toArray(String[]::new);
+                int mark = (int) Double.parseDouble(values[markPosition].replaceAll("[\\[\\] ]", "").trim());
+                int d = values.length - 1;
+                float[] positionPoint = new float[d];
+
+                for (int j = 0; j < d; j++) {
+                    positionPoint[j] = Float.parseFloat(values[j + (markPosition + 1) % (d + 1)].replaceAll("[\\[\\] ]", ""));
+                }
+                Point point = new Point(i++, positionPoint, mark, -1, -1);
+                pointList.add(point);
+//                String key = values[mark].trim();
+//                countMap.put(key, countMap.getOrDefault(key, 0) + 1);
+                label_numbers.add(mark);
+            }
+            OutPut.k = label_numbers.size();
+//            System.out.println("Value\tCount");
+//            for (Map.Entry<String, Integer> entry : countMap.entrySet()) {
+//                System.out.println(entry.getKey() + "\t" + entry.getValue());
+//            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return pointList;
+    }
+}
